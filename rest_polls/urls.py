9378299bd44c95d51ddc6071c73458247986d55f
podwar2008 <podwar2008@gmail.com>
2021-06-29 01:07:17 +0300

    Add swagger

--- rest_polls/urls.py
+++ rest_polls/urls.py
@@ -15,10 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from api.views import admin_views
 from api.views import client_views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='LMS API',
+        default_version='v1',
+        description='API for LMS project',
+        contact=openapi.Contact(email='podwar2008@gmail.com'),
+        url='http://127.0.0.1:8000/api/v1/',
+    ),
+    public=False,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 admin_patterns = ([
@@ -34,6 +48,7 @@ client_patterns = ([
 
 
 urlpatterns = [
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api-admin/', include(admin_patterns)),
     path('api-client/', include(client_patterns)),
